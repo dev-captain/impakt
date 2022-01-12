cf14ce5ed553f23dcc4ca3d0a3f8bca9a56ec41d
dev-captain <luckydeveloper1113@gmail.com>
2022-01-12 21:52:54 +0300

    fix: mixed text issue on tablet mode

--- src/components/ui/home/CountdownHero/index.tsx
+++ src/components/ui/home/CountdownHero/index.tsx
@@ -1,5 +1,5 @@
 import { memo } from 'react';
-import { HStack, SimpleGrid, Text, VStack } from '@chakra-ui/react';
+import { HStack, SimpleGrid, Text, useMediaQuery, VStack } from '@chakra-ui/react';
 import useCountdown from 'hooks/useCountdown';
 import HeroLayout from 'components/layouts/HeroLayout';
 import Logo from 'components/core/Logo';
@@ -10,12 +10,22 @@ import CounterItem from './CounterItem';
 const CountdownHero = ({ date }: { date: Date }) => {
   const countdown = useCountdown(date);
   const items = ['days', 'hours', 'minutes', 'seconds'];
+  const [isLessThan991] = useMediaQuery('(max-width: 992px)');
+  const [isLargerThan767] = useMediaQuery('(min-width: 767px)');
 
   return (
     <HeroLayout hideBlur pos="absolute">
       <VStack pos="absolute" top={{ base: 4, md: 16 }} zIndex={100}>
         <SocialMediasArea />
-        <HStack zIndex={2} paddingTop="10">
+        <HStack
+          zIndex={2}
+          alignItems="center"
+          paddingTop={{
+            base: '8',
+            md: '10',
+            xl: '16',
+          }}
+        >
           <Logo />
           <Text fontSize="54px" fontWeight="semibold" paddingLeft={2} paddingBottom={1}>
             in:
@@ -30,7 +40,8 @@ const CountdownHero = ({ date }: { date: Date }) => {
         zIndex={2}
         paddingTop={{
           base: '200px',
-          md: '24',
+          md: isLargerThan767 && isLessThan991 ? '200px' : '10',
+          xl: '24',
         }}
       >
         {items.map((item) => (
