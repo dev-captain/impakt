428c7d9eb8ae695e5b9086d0ea3e3b92cd282366
dev-captain <luckydeveloper1113@gmail.com>
2022-01-26 13:21:11 +0300

    chore: revert

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,14 @@
-import { lazy, Suspense, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Container } from '@chakra-ui/layout';
 import { useColorMode } from '@chakra-ui/react';
-
-const BurnToEarnHero = lazy(() => import('components/ui/home/BurnToEarnHero'));
-const ImpaktGamesHero = lazy(() => import('components/ui/home/ImpaktGamesHero'));
-const MotionCaptureHero = lazy(() => import('components/ui/home/MotionCaptureHero'));
-const PartnerAndInvestorHero = lazy(() => import('components/ui/home/PartnersAndInvestorHero'));
-const Footer = lazy(() => import('components/core/Footer'));
-const Contact = lazy(() => import('components/ui/home/Contact'));
+import {
+  Footer,
+  BurnToEarnHero,
+  ImpaktGamesHero,
+  MotionCaptureHero,
+  PartnerAndInvestorHero,
+} from 'components/ui/home';
+import Contact from 'components/ui/home/Contact';
 
 const HomePage = () => {
   const { setColorMode } = useColorMode();
@@ -18,36 +19,27 @@ const HomePage = () => {
 
   return (
     <Container spacing={0} p={0} minW="full" m={0}>
-      <Suspense fallback={<div />}>
-        <div id="impakt-games">
-          <ImpaktGamesHero />
-        </div>
-      </Suspense>
+      <div id="impakt-games">
+        <ImpaktGamesHero />
+      </div>
 
-      <Suspense fallback={<div />}>
-        <div id="motion-capture">
-          <MotionCaptureHero />
-        </div>
-      </Suspense>
+      <div id="motion-capture">
+        <MotionCaptureHero />
+      </div>
 
-      <Suspense fallback={<div />}>
-        <div id="burn-to-earn">
-          <BurnToEarnHero />
-        </div>
-      </Suspense>
-      <Suspense fallback={<div />}>
-        <div id="partner-and-investor">
-          <PartnerAndInvestorHero />
-        </div>
-      </Suspense>
-      <Suspense fallback={<div />}>
-        <div id="contact">
-          <Contact />
-        </div>
-      </Suspense>
-      <Suspense fallback={<div />}>
-        <Footer />
-      </Suspense>
+      <div id="burn-to-earn">
+        <BurnToEarnHero />
+      </div>
+
+      <div id="partner-and-investor">
+        <PartnerAndInvestorHero />
+      </div>
+
+      <div id="contact">
+        <Contact />
+      </div>
+
+      <Footer />
     </Container>
   );
 };
