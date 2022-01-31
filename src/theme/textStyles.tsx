3fd94b9be40f1bb301698a61277ddb95565dfb81
dev-captain <luckydeveloper1113@gmail.com>
2022-01-31 09:12:50 +0300

    feat: added theme configs

--- src/theme/textStyles.tsx
+++ src/theme/textStyles.tsx
@@ -1,40 +1,100 @@
 import { theme } from '@chakra-ui/react';
 
 export default {
-  black120: {
+  black10: {
     fontSize: '120px',
     fontWeight: '900',
     lineHeight: '114px',
   },
-  black98: {
-    fontSize: '98px',
+  black9: {
+    fontSize: '90px',
     fontWeight: '900',
     lineHeight: '90px',
   },
-  black72: {
+  black8: {
     fontSize: '72px',
     fontWeight: '900',
     lineHeight: '72px',
   },
-  bold56: {
+  bold7: {
     fontSize: '56px',
     fontWeight: '700',
     lineHeight: '60px',
   },
-  bold40: {
-    fontSize: '40px',
-    fontWeight: '700',
-    lineHeight: '40px',
-  },
-  light56: {
+  light7: {
     fontSize: '56px',
     fontWeight: '300',
     lineHeight: '60px',
   },
-  light40: {
+  bold6: {
+    fontSize: '40px',
+    fontWeight: '700',
+    lineHeight: '40px',
+  },
+  regular6: {
+    fontSize: '40px',
+    fontWeight: '400',
+    lineHeight: '40px',
+  },
+  light6: {
     fontSize: '40px',
     fontWeight: '300',
     lineHeight: '40px',
+  },
+  bold5: {
+    fontSize: '32px',
+    fontWeight: '700',
+    lineHeight: '36px',
+  },
+  regular5: {
+    fontSize: '32px',
+    fontWeight: '400',
+    lineHeight: '36px',
+  },
+  bold4: {
+    fontSize: '24px',
+    fontWeight: '700',
+    lineHeight: '32px',
+  },
+  regular4: {
+    fontSize: '24px',
+    fontWeight: '400',
+    lineHeight: '32px',
+  },
+  bold3: {
+    fontSize: '16px',
+    fontWeight: '700',
+    lineHeight: '24px',
+  },
+  semiBold3: {
+    fontSize: '16px',
+    fontWeight: '600',
+    lineHeight: '24px',
+  },
+  regular3: {
+    fontSize: '16px',
+    fontWeight: '400',
+    lineHeight: '24px',
+  },
+  bold2: {
+    fontSize: '14px',
+    fontWeight: '700',
+    lineHeight: '20px',
+  },
+  regular2: {
+    fontSize: '12px',
+    fontWeight: '400',
+    lineHeight: '16px',
+  },
+  bold1: {
+    fontSize: '12px',
+    fontWeight: '700',
+    lineHeight: '16px',
+  },
+  regular1: {
+    fontSize: '14px',
+    fontWeight: '400',
+    lineHeight: '20px',
   },
   regular20: {
     fontSize: '20px',
@@ -56,7 +116,6 @@ export default {
     fontWeight: '400',
     lineHeight: '16px',
   },
-
   counterNumber: {
     fontFamily: 'Source Sans Pro',
     fontWeight: '300',
