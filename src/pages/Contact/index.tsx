c131f332206f095bee80da56e921e79c51406ce8
dev-captain <luckydeveloper1113@gmail.com>
2022-02-16 14:13:32 +0300

    fix: white line between two sectin in contact page

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,12 +1,15 @@
+import { useColorModeValue, VStack } from '@chakra-ui/react';
 import DiscordSupport from './component/DiscordSupport';
 import SendMessage from './component/SendMessage';
 
 const ContactPage = () => {
+  const bgColor = useColorModeValue('glass.800', 'glass.300');
+
   return (
-    <>
+    <VStack spacing={0} bgColor={bgColor}>
       <SendMessage />
       <DiscordSupport />
-    </>
+    </VStack>
   );
 };
 
