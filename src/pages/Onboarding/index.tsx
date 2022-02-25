3df76a037a07b4ec5e0d6eac25c45bd97eb3dbbd
dev-captain <luckydeveloper1113@gmail.com>
2022-02-25 13:47:24 +0300

    feat: added expire time for code and refactored code

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -1,45 +1,35 @@
-import { Spinner, Text, useColorModeValue, VStack } from '@chakra-ui/react';
+import { Spinner, Text, VStack } from '@chakra-ui/react';
 import HeroLayout from 'components/layouts/HeroLayout';
 import { Widget } from '@typeform/embed-react';
 import useOnboardingCode from 'hooks/useOnboardingCode';
-import { useState } from 'react';
 import { GradientEllipse } from 'components/ui/home/RoadmapHero/Gradients';
 import { layoutPadding } from 'theme';
 import Container from './component/Container';
 
 const OnboardingPage = () => {
-  const [ready, setReady] = useState(false);
-  const text = useColorModeValue('glass.100', 'glass.800');
-  const { loading, isValid, isCodeExist } = useOnboardingCode();
-  const bg = useColorModeValue('glass.700', 'glass.300');
+  const {
+    bg,
+    text,
+    showWidget,
+    widgetProps,
+    showSpinner,
+    informativeMessage,
+    showInformativeMessage,
+  } = useOnboardingCode();
 
   return (
     <HeroLayout showFooter showNavbar minH="60vh">
       <VStack w="full" minH="70vh" pt={{ base: '16px' }} color={text} px={layoutPadding}>
-        {(loading || (!ready && isValid)) && <Spinner size="xl" />}
-        {!loading && !isValid && (
+        {showSpinner && <Spinner size="xl" />}
+        {showInformativeMessage && (
           <Container bgColor={bg} textColor={text}>
             <Text textStyle="bold6" fontWeight="700" pl="16px" align="center">
-              {isCodeExist
-                ? 'Your onboarding code is wrong.'
-                : 'Your onboarding code is not found.'}
+              {informativeMessage}
             </Text>
             <GradientEllipse />
           </Container>
         )}
-        {!loading && isValid && (
-          <Widget
-            id="EEnQpuTA"
-            style={{
-              width: '100%',
-              padding: '16px',
-              height: ready ? '70vh' : '0vh',
-            }}
-            onReady={() => {
-              setReady(true);
-            }}
-          />
-        )}
+        {showWidget && <Widget {...widgetProps} />}
       </VStack>
     </HeroLayout>
   );
