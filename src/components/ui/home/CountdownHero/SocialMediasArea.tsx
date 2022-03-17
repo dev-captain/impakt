b0d71aaceecf0ffb1157b2fbe9efa134c4e58373
dev-captain <luckydeveloper1113@gmail.com>
2022-03-17 20:49:36 +0900

    chore: update discord link

--- src/components/ui/home/CountdownHero/SocialMediasArea.tsx
+++ src/components/ui/home/CountdownHero/SocialMediasArea.tsx
@@ -5,7 +5,7 @@ import SocialIcon from './SocialIcon';
 const SocialMediasArea = () => {
   return (
     <HStack top={8} zIndex={10} spacing={10} paddingTop={{ base: 18, md: 0 }}>
-      <SocialIcon name="Discord" href="https://discord.gg/qhZ3rTNB" />
+      <SocialIcon name="Discord" href="https://discord.gg/eVQJ8XRa9S" />
       <SocialIcon name="Twitter" href="https://twitter.com/demideuszin" />
     </HStack>
   );
