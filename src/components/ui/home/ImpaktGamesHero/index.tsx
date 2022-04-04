498d72f96a4970676516221d42cbba9cb2d37974
dev-captain <luckydeveloper1113@gmail.com>
2022-04-04 18:46:12 +0530

    Download link changes

--- src/components/ui/home/ImpaktGamesHero/index.tsx
+++ src/components/ui/home/ImpaktGamesHero/index.tsx
@@ -47,14 +47,12 @@ const ImpaktGamesHero = () => {
                   iconName="Windows"
                   title="Download for Windows"
                   link="https://discord.gg/e38WmMRK"
-                  // link="https://impakt-build-artifacts.s3.us-east-2.amazonaws.com/Windows/Impakt_Windows_v1.1.8.zip"
                 />
                 <DownloadButton
                   isHorizontal
                   iconName="Apple"
                   title="Download for Mac"
                   link="https://discord.gg/e38WmMRK"
-                  // link="https://impakt-build-artifacts.s3.us-east-2.amazonaws.com/Mac/Impakt_Mac_v1.1.8.zip"
                 />
               </Wrapper>
             </VStack>
