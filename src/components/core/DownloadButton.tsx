c4585dcd8d0059f4e3da0981ca3df720e8aedbfc
dev-captain <luckydeveloper1113@gmail.com>
2022-04-08 21:41:08 +0530

    All button link should open in new tab

--- src/components/core/DownloadButton.tsx
+++ src/components/core/DownloadButton.tsx
@@ -26,6 +26,8 @@ const DownloadButton = ({
       borderRadius="20px"
       bg="linear-gradient(143.78deg, #DC143C 18.94%, #B22222 78.86%)"
       href={link}
+      target="_blank"
+      _focus={{}}
       className="buttonHover"
     >
       <Wrapper>
