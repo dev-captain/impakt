82da36a30329aa20106aaa066ca9c310990deff0
dev-captain <luckydeveloper1113@gmail.com>
2022-04-21 19:15:39 +0900

    refactor: gradient button props

--- src/components/core/GradientButton.tsx
+++ src/components/core/GradientButton.tsx
@@ -1,49 +1,39 @@
-import { Button, HStack, Text } from '@chakra-ui/react';
+import { Button, ButtonProps, HStack, Text } from '@chakra-ui/react';
 import Icons from 'components/icons';
+
+interface GradientButtonProps extends ButtonProps {
+  title: string;
+  icon?: string;
+  radius?: string;
+  color?: string;
+}
 
 const GradientButton = ({
   title,
   icon,
   w = 'auto',
-  bgGradient,
-  py,
-  px,
-  minW,
-  radius,
-  d,
-  disabled,
+  bgGradient = 'linear-gradient(150.95deg, #363639 15.07%, #222226 82.14%)',
+  radius = '20px',
+  px = '3rem',
+  py = '2rem',
+  minW = '160px',
   color = 'white',
-  onClick,
-}: {
-  title: string;
-  icon?: string;
-  w?: string | any;
-  bgGradient?: string;
-  px?: string;
-  py?: string;
-  radius?: string;
-  minW?: string;
-  d?: any;
-  color?: string;
-  disabled?: boolean;
-  onClick?: () => void;
-}) => {
+  ...buttonProps
+}: GradientButtonProps) => {
   const Icon = icon ? Icons?.[icon] : undefined;
 
   return (
     <Button
       w={w}
-      d={d}
+      px={px}
+      py={py}
       size="lg"
-      onClick={onClick}
-      px={px || '3rem'}
-      py={py || '2rem'}
-      disabled={disabled}
-      minW={minW || '160px'}
-      borderRadius={radius || '20px'}
+      minW={minW}
+      borderRadius={radius}
       boxShadow="0px 4px 4px rgba(0, 0, 0, 0.15), 0px 4px 14px rgba(0, 0, 0, 0.16)"
       bgGradient={bgGradient || 'linear-gradient(150.95deg, #363639 15.07%, #222226 82.14%)'}
       className="buttonHover"
+      {...buttonProps}
     >
       <HStack spacing={2} alignItems="center" justifyContent="center">
         {Icon && <Icon />}
