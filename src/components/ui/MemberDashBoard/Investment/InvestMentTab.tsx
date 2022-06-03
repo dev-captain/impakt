75d1c9035a987377d3444b332749d70dfd94e4a0
dev-captain <luckydeveloper1113@gmail.com>
2022-06-03 17:32:37 +0300

    contents of phases done

--- src/components/ui/MemberDashBoard/Investment/InvestMentTab.tsx
+++ src/components/ui/MemberDashBoard/Investment/InvestMentTab.tsx
@@ -89,16 +89,64 @@ const InvestMentTab: React.FC<InvestmentTabPropsI> = ({
                 </PhaseDescriptionText>
                 {activeTabIndex === 0 && <ActiveMembersCard activeMembersValue={8013} />}
                 <PhaseDescriptionText>
-                  <b>Who knows how low the final price will be?</b>
-                  Token price starts at $0.01 USD and the final price will be determined by the
-                  number of active members on Impakt. A member is deemed active in this phase, by
-                  doing at least one daily, weekly or targeted challenge every week.
+                  <b>Who knows how low the final price will be?</b> Token price starts at $0.01 USD
+                  and the final price will be determined by the number of active members on Impakt.
+                  A member is deemed active in this phase, by doing at least one daily, weekly or
+                  targeted challenge every week.
                 </PhaseDescriptionText>
                 <PhaseDescriptionText>
                   <b>
                     So grab your friends, get fit and have fun together! It&apos;ll be great for
                     everyone!
                   </b>
+                </PhaseDescriptionText>
+              </VStack>
+            </AnimationInWhenVisible>
+          </TabPanel>
+          <TabPanel position="relative" padding="0 !important">
+            <AnimationInWhenVisible animationType="fade">
+              <VStack rowGap="32px" w="full">
+                <Box id="headline" display="flex" alignItems="center" w="100%">
+                  <Box position="relative" alignItems="center" display="flex">
+                    <PhaseHeadlineText>Phase 2: Community Private Presale</PhaseHeadlineText>
+                  </Box>
+                </Box>
+                <PhaseDescriptionText>
+                  <b>Don&apos;t be &quot;just a member&quot;... Be Family! </b> At Impakt, we take
+                  care of our own first. In this phase, you, as a member of the Impakt community
+                  will be able to convert your hard earned GODL into token allocation. You&apos;ll
+                  be able to invest up to the amount of your allocation in our virtual token, $vIO.
+                </PhaseDescriptionText>
+                <PhaseDescriptionText>
+                  At the beginning of this phase $vIO will start at the price unlocked in Phase 1
+                  and then increase until the price of our IDO in the next phase. So essentially,
+                  the sooner you invest in your presale, the more tokens you will get for your
+                  money.
+                </PhaseDescriptionText>
+                <PhaseDescriptionText>
+                  <b>Note:</b> Each $vIO converts to $IO. $IO is the official Impakt blockchain
+                  token.
+                </PhaseDescriptionText>
+              </VStack>
+            </AnimationInWhenVisible>
+          </TabPanel>
+          <TabPanel position="relative" padding="0 !important">
+            <AnimationInWhenVisible animationType="fade">
+              <VStack rowGap="32px" w="full">
+                <Box id="headline" display="flex" alignItems="center" w="100%">
+                  <Box position="relative" alignItems="center" display="flex">
+                    <PhaseHeadlineText>Phase 3: IDO Token Launch</PhaseHeadlineText>
+                  </Box>
+                </Box>
+                <PhaseDescriptionText>
+                  <b>
+                    This is the time we&apos;ve all been waiting for! Once our token $IO is
+                    launched, your $vIO translate 1:1 to $IO.
+                  </b>{' '}
+                </PhaseDescriptionText>
+                <PhaseDescriptionText>
+                  Your tokens will be locked for 3 months from IDO, and after that, linearly vest
+                  over a 6 month period. You will see the tokens being unlocked in real time.
                 </PhaseDescriptionText>
               </VStack>
             </AnimationInWhenVisible>
