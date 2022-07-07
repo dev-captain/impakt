537ffb4ef5e0d4a5786dee80c7d5a02950dbaccc
dev-captain <luckydeveloper1113@gmail.com>
2022-07-07 09:20:08 +0300

    make disseaper other section once is animate false

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,31 +8,38 @@ import {
   HeroSection,
   EarnCrypto,
 } from 'components/ui/home';
+import useAppSelector from '../../hooks/useAppSelector';
 
 const HomePage = () => {
+  const isAnimated = useAppSelector((state) => state.stateReducer.heroVideo.isAnimated);
+
   return (
     <Container spacing={0} p={0} minW="full" m={0} bgColor="">
       <HeroSection />
 
-      <div>
-        <EarnCrypto />
-      </div>
+      {isAnimated && (
+        <>
+          <div>
+            <EarnCrypto />
+          </div>
 
-      <div>
-        <ImpaktNFT />
-      </div>
-      <div>
-        <ImpaktCreed />
-      </div>
-      <div>
-        <Founder />
-      </div>
-      <div>
-        <Advisor />
-      </div>
-      <div>
-        <ImpaktFooter />
-      </div>
+          <div>
+            <ImpaktNFT />
+          </div>
+          <div>
+            <ImpaktCreed />
+          </div>
+          <div>
+            <Founder />
+          </div>
+          <div>
+            <Advisor />
+          </div>
+          <div>
+            <ImpaktFooter />
+          </div>
+        </>
+      )}
       {/* <div>
         <CardFounder />
       </div> */}
