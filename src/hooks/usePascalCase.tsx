6c2d34071d3e32b244b5c6d3294e1d83277df6a4
dev-captain <luckydeveloper1113@gmail.com>
2022-07-18 10:58:51 +0530

    Please make sure the exercises are named exactly the same as in the app

--- src/hooks/usePascalCase.tsx
+++ src/hooks/usePascalCase.tsx
@@ -4,7 +4,7 @@ const usePascalCase = (label: string) => {
   const [excerciseLable, setExcerciseLable] = useState<string>();
   useEffect(() => {
     if (label) {
-      setExcerciseLable(label.replace(/([a-z0-9])([A-Z])/g, '$1 $2'));
+      setExcerciseLable(label.replace(/([a-z0-9])([A-Z])/g, '$1 $2').toUpperCase());
     }
   }, [excerciseLable]);
   return excerciseLable;
