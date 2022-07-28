e90d3b87dc31a24334c461d5bd9c9c37c101496c
dev-captain <luckydeveloper1113@gmail.com>
2022-07-28 11:15:36 +0300

    wallet address refactored & splitted code

---
+++ src/components/ui/MemberDashBoard/WalletAddressModal/WalletAddressForm.tsx
@@ -0,0 +1,56 @@
+import { Box, FormControl, HStack } from '@chakra-ui/react';
+import * as React from 'react';
+import { Common, I } from 'components';
+
+import { useForm } from 'hooks';
+
+const WalletAddressForm: React.FC = () => {
+  const {
+    // handleSubmit,
+    getValues,
+    setValue,
+  } = useForm({
+    defaultValues: { walletAddress: '' },
+  });
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    setValue(e.target.name as any, e.target.value as any, { shouldValidate: true });
+  };
+
+  return (
+    <FormControl
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDir="column"
+      m="0 !important"
+      rowGap="32px"
+      // onSubmit={handleSubmit()}
+      as="form"
+      autoComplete="off"
+      w="full"
+    >
+      <HStack spacing={2} w="full" flexWrap={{ base: 'wrap', md: 'nowrap' }}>
+        <Box w="full">
+          <Common.InputGroup
+            placeholder="Enter wallet address"
+            name="walletAddress"
+            onChange={onChange}
+            leftIcon={<I.EthIcon />}
+          />
+        </Box>
+        <Box w="28%">
+          <Common.ImpaktButton
+            disabled={!getValues('walletAddress')}
+            minW={{ base: '100%', md: '137px' }}
+            size="lg"
+          >
+            Submit
+          </Common.ImpaktButton>
+        </Box>
+      </HStack>
+    </FormControl>
+  );
+};
+
+export default WalletAddressForm;
--- src/components/ui/MemberDashBoard/WalletAddressModal/WalletAddressModal.tsx
+++ src/components/ui/MemberDashBoard/WalletAddressModal/WalletAddressModal.tsx
@@ -14,7 +14,12 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 import React from 'react';
+import { Common } from 'components';
+import { useForm } from 'hooks';
+
 import Images from '../../../../assets/images';
+import { EthIcon } from '../../../icons';
+import WalletAddressForm from './WalletAddressForm';
 
 interface InfoModalPropsI {
   handleModal?: () => void;
@@ -25,7 +30,7 @@ const WalletAddressModal: React.FC<InfoModalPropsI> = ({ handleModal, isModalOpe
   const { onClose } = useDisclosure();
 
   return (
-    <Modal closeOnOverlayClick={false} isOpen={isModalOpen} onClose={onClose} isCentered>
+    <Modal closeOnOverlayClick isOpen={isModalOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent maxW="720px" bgColor="#1C1C28" borderRadius="32px">
         <ModalHeader textStyle="bold4" color="#FFF" px="40px" pt="25px" pb="15px">
@@ -37,60 +42,7 @@ const WalletAddressModal: React.FC<InfoModalPropsI> = ({ handleModal, isModalOpe
           onClick={handleModal}
         />
         <ModalBody pt="0px" px="40px" pb="40px">
-          <HStack spacing={4} w="full" flexWrap={{ base: 'wrap', md: 'nowrap' }}>
-            <Box w="full">
-              <InputGroup border="none" background="#121216" borderRadius="12px">
-                <InputLeftElement
-                  pointerEvents="none"
-                  height="60px"
-                  width="60px"
-                  borderEnd="1px solid rgba(255, 255, 255, 0.1)"
-                  marginRight="16px"
-                >
-                  <img
-                    src={Images.Common.Walletad}
-                    style={{ opacity: '0.5' }}
-                    alt="Discord"
-                    width="19px"
-                    height="32px"
-                  />
-                </InputLeftElement>
-                <Input
-                  border="none"
-                  type="address"
-                  height="60px"
-                  pl="80px"
-                  minWidth={{ base: '100%', md: '495px' }}
-                  placeholder="Enter wallet address"
-                  color="rgba(255, 255, 255, 0.4)"
-                  textStyle="regular201"
-                  _placeholder={{ color: 'rgba(255, 255, 255, 0.4)' }}
-                  _focus={{ border: 'none !important', boxShadow: 'none !important' }}
-                  borderRadius="16px !important"
-                />
-              </InputGroup>
-            </Box>
-            <Box
-              w="full"
-              mt={{ base: '16px !important', md: '0px !important' }}
-              ms={{ base: '0px !important', md: '16px !important' }}
-            >
-              <Button
-                background="linear-gradient(180deg, #F04153 0%, #F91F35 100%)"
-                _hover={{ background: 'linear-gradient(180deg, #F04153 0%, #F91F35 100%)' }}
-                color="#FFF"
-                fontSize="20px"
-                lineHeight="32px"
-                fontWeight="500"
-                paddingX="32px"
-                height="60px"
-                borderRadius="16px"
-                minWidth={{ base: '100%', md: '137px' }}
-              >
-                Submit
-              </Button>
-            </Box>
-          </HStack>
+          <WalletAddressForm />
         </ModalBody>
       </ModalContent>
     </Modal>
