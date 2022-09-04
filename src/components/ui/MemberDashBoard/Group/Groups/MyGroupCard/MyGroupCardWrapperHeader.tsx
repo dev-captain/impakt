27b8b6e78b1da18081f3551dc403d2073e88d291
dev-captain <luckydeveloper1113@gmail.com>
2022-09-04 11:15:52 +0300

    my groups section statistic  fix

--- src/components/ui/MemberDashBoard/Group/Groups/MyGroupCard/MyGroupCardWrapperHeader.tsx
+++ src/components/ui/MemberDashBoard/Group/Groups/MyGroupCard/MyGroupCardWrapperHeader.tsx
@@ -1,28 +1,33 @@
 import * as React from 'react';
 
 import { I } from 'components';
+import { useAppSelector } from 'hooks';
 import MyGroupCardWrapperHeaderStatisticTag from './MyGroupCardWrapperHeaderStatisticTag';
 import GroupCardWrapperHeader from '../GroupCardWrapperHeader';
 
 const MyGroupCardWrapperHeader: React.FC = () => {
+  const member = useAppSelector((state) => state.memberAuth.member);
+  const myGroups = useAppSelector((state) => state.groupsReducer.myGroups);
+  const ownedGroup = myGroups.filter((group) => group.ownerId === member?.id);
+
   return (
     <GroupCardWrapperHeader title="Your Groups">
       <MyGroupCardWrapperHeaderStatisticTag
-        value="1"
+        value={myGroups.length.toString()}
         icon={<I.PeopleIcon />}
         name="Joined Groups"
         color="#1C1C28"
         width="60px"
       />
-      <MyGroupCardWrapperHeaderStatisticTag
+      {/* <MyGroupCardWrapperHeaderStatisticTag
         value="0"
         icon={<I.RewardIcon color="#D92D85" />}
         name="Challenges completed"
         color="#D92D85"
         width="94px"
-      />
+      /> */}
       <MyGroupCardWrapperHeaderStatisticTag
-        value="1"
+        value={ownedGroup.length.toString()}
         icon={<I.AddMemberIcon color="#5714EC" width="32px" height="32px" />}
         name="Groupscreated"
         color="#5714EC"
