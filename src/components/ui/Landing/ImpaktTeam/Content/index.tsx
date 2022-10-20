d6b264fd6ec6c692059e0695f7752c6b0c6e7728
dev-captain <luckydeveloper1113@gmail.com>
2022-10-20 14:08:33 +0300

    with counter

--- src/components/ui/Landing/ImpaktTeam/Content/index.tsx
+++ src/components/ui/Landing/ImpaktTeam/Content/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import ImpaktTeamAdvisors from './ImpaktTeamAdvisors';
 import ImpaktTeamFounders from './ImpaktTeamFounders';
+import ImpaktTeamMemberCounter from './ImpaktTeamMemberCounter';
 
 const ImpaktTeamContent: React.FC = () => {
   return (
     <>
       <ImpaktTeamFounders />
       <ImpaktTeamAdvisors />
+      <ImpaktTeamMemberCounter />
     </>
   );
 };
