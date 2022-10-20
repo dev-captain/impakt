3b720c84827dea58ed586e23dd18c6d28b87a199
dev-captain <luckydeveloper1113@gmail.com>
2022-10-20 14:06:24 +0300

    with counter

---
+++ src/components/ui/Landing/ImpaktTeam/Content/ImpaktTeamMemberCounter.tsx
@@ -0,0 +1,36 @@
+import { Box, Text } from '@chakra-ui/react';
+import * as React from 'react';
+import CountUp from 'react-countup';
+
+const ImpaktTeamMemberCounter: React.FC = () => {
+  return (
+    <Box mt={{ base: '20px !important', lg: '80px !important' }} display="flex" flexDir="column">
+      <Text
+        width="100%"
+        color="#35485a"
+        fontSize="36px"
+        fontWeight="700"
+        textTransform="uppercase"
+        textAlign="center"
+      >
+        TEAM MEMBERS
+      </Text>
+      <CountUp start={0} end={40} duration={5} separator=" " enableScrollSpy prefix="" suffix="+ ">
+        {({ countUpRef }) => (
+          <Box
+            as="span"
+            width="100%"
+            color="#35485a"
+            fontSize="96px"
+            fontWeight="700"
+            textTransform="uppercase"
+            textAlign="center"
+            mt={{ base: '20px !important', lg: '0 !important' }}
+            ref={countUpRef as any}
+          />
+        )}
+      </CountUp>
+    </Box>
+  );
+};
+export default ImpaktTeamMemberCounter;
