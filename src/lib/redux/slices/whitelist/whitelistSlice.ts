1f6a26cbf0b9e4dd4a58192d23df8e471419f163
dev-captain <luckydeveloper1113@gmail.com>
2022-10-27 09:17:17 +0300

    is whitelisted moved

--- src/lib/redux/slices/whitelist/whitelistSlice.ts
+++ src/lib/redux/slices/whitelist/whitelistSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { getWhiteListed } from './actions/getWhiteListed';
 
 interface WhitelistInitialI {
   isWhitelisted: boolean;
@@ -15,21 +14,6 @@ const whitelistSlice = createSlice({
   name: 'whitelist',
   initialState: whitelistInitialState as WhitelistInitialI,
   reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(getWhiteListed.pending, (state) => {
-        state.isWhitelisted = false;
-        state.isWhitelistingLoading = true;
-      })
-      .addCase(getWhiteListed.fulfilled, (state, action) => {
-        state.isWhitelisted = action.payload;
-        state.isWhitelistingLoading = false;
-      })
-      .addCase(getWhiteListed.rejected, (state) => {
-        state.isWhitelisted = false;
-        state.isWhitelistingLoading = false;
-      });
-  },
 });
 
 // eslint-disable-next-line import/prefer-default-export
--- src/pages/MemberDashBoard/index.tsx
+++ src/pages/MemberDashBoard/index.tsx
@@ -15,7 +15,6 @@ import { fetchReferralsRewardKoin } from '../../lib/redux/slices/referrals/actio
 import { usePersistedAuthStore, usePersistedBalanceScoreStore } from '../../lib/zustand';
 import { useGodlAccountControllerGetAccount } from '../../lib/impakt-dev-api-client/react-query/godl/godl';
 import { useCoinAccountControllerV1GetAccount } from '../../lib/impakt-dev-api-client/react-query/coin/coin';
-// import { useRewardHistoryControllerV1GetRewardHistory } from '../../lib/impakt-dev-api-client/react-query/default/default';
 // import { VStack } from '@chakra-ui/react';
 // import ExerciseHistory from 'components/ui/MemberDashBoard/ExerciseHistory/ExerciseHistory';
 // import HeroLayout from '../../components/layouts/HeroLayout';
@@ -23,43 +22,35 @@ import { useCoinAccountControllerV1GetAccount } from '../../lib/impakt-dev-api-c
 // import MemberWhitelistLeaderBoard from '../../components/ui/MemberDashBoard/MemberWhiteListLeaderBoard/MemberWhiteListLeaderBoard';
 // import ReferralsAndWhiteListChallange from '../../components/ui/MemberDashBoard/ReferralsAndWhiteListChallange/ReferralsAndWhiteListChallange';
 // import MemberDashboardCard from '../../components/ui/MemberDashBoard/MemberDashBoardCard';
+// import { useUserControllerIsWhitelisted } from '../../lib/impakt-dev-api-client/react-query/users/users';
+// import { useRewardHistoryControllerV1GetRewardHistory } from '../../lib/impakt-dev-api-client/react-query/default/default';
 
 const MemberDashboard: React.FC = () => {
   const { member } = usePersistedAuthStore();
   const store = usePersistedBalanceScoreStore();
-  const fetchGodlBalanceScore = useGodlAccountControllerGetAccount();
-  const fetchKoinBalanceScore = useCoinAccountControllerV1GetAccount();
+  const fetchGodlBalanceScoreQuery = useGodlAccountControllerGetAccount();
+  const fetchKoinBalanceScoreQuery = useCoinAccountControllerV1GetAccount();
   // const fetchRewardHistory = useRewardHistoryControllerV1GetRewardHistory();
+  // const fetchIsUserWhitelistedQuery = useUserControllerIsWhitelisted();
 
   const dispatch = useAppDispatch();
 
   React.useEffect(() => {
-    if (fetchGodlBalanceScore.isFetched) {
-      store.setGodlBalanceScore(fetchGodlBalanceScore.data?.balance ?? 0);
+    if (fetchGodlBalanceScoreQuery.isFetched) {
+      store.setGodlBalanceScore(fetchGodlBalanceScoreQuery.data?.balance ?? 0);
     }
-  }, [fetchGodlBalanceScore.isFetched]);
+  }, [fetchGodlBalanceScoreQuery.isFetched]);
 
   React.useEffect(() => {
-    if (fetchKoinBalanceScore.isFetched) {
-      store.setKoinBalanceScore(fetchKoinBalanceScore.data?.balance ?? 0);
+    if (fetchKoinBalanceScoreQuery.isFetched) {
+      store.setKoinBalanceScore(fetchKoinBalanceScoreQuery.data?.balance ?? 0);
     }
-  }, [fetchKoinBalanceScore.isFetched]);
+  }, [fetchKoinBalanceScoreQuery.isFetched]);
 
   React.useEffect(() => {
     if (!member) return;
     dispatch(fetchActiveDays(member.id));
   }, []);
-
-  // React.useEffect(() => {
-  //   if (!member) return;
-  //   dispatch(getWhiteListed());
-  // }, []);
-
-  // React.useEffect(() => {
-  //   if (member) {
-  //     fetchRewardHistory('User', member.id);
-  //   }
-  // }, []);
 
   // React.useEffect(() => {
   //   if (!member) return;
