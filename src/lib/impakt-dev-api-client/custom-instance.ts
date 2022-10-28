4900f28fb53eeed4160bc88dad39f368ea228fbe
dev-captain <luckydeveloper1113@gmail.com>
2022-10-28 11:07:58 +0300

    refresh token expiration fix for custom instance

--- src/lib/impakt-dev-api-client/custom-instance.ts
+++ src/lib/impakt-dev-api-client/custom-instance.ts
@@ -1,39 +1,31 @@
 // custom-instance.ts
 import Axios, { AxiosError, AxiosRequestConfig } from 'axios';
-import { authControllerSignAccessToken } from './react-query/auth/auth';
-
-const API_SERVER_BASE_URL = process.env.REACT_APP_API_BASE_URL ?? '';
-export const AXIOS_INSTANCE = Axios.create({
-  baseURL: API_SERVER_BASE_URL,
-  withCredentials: true,
-}); // use your own URL here or environment variable
-
-// add a second `options` argument here if you want to pass extra options to each generated query
+import { usePersistedAuthStore } from '../zustand';
 
 export const customInstance = <T>(
   config: AxiosRequestConfig,
   options?: AxiosRequestConfig,
 ): Promise<T> => {
   const source = Axios.CancelToken.source();
+  // eslint-disable-next-line prefer-const
+  // 1200
 
   AXIOS_INSTANCE.interceptors.response.use(
     (response) => {
       return response;
     },
-    async (error: AxiosError) => {
-      const originalRequest = error.config;
-      if (error.response?.status === 403 && (!originalRequest as any).retry) {
-        (originalRequest as any).retry = true;
-        try {
-          await refreshAccessToken();
-        } catch (e) {
-          Promise.reject(error);
-        }
+    async (error: any) => {
+      const originalRequest = error.config as any;
+      if (error.response?.status === 401 && !originalRequest.retry) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        originalRequest.retry = true;
+        await AXIOS_INSTANCE_REFRESH_TOKEN.post('/api/v1/iam/auth/refresh');
+        AXIOS_INSTANCE(originalRequest);
 
-        return AXIOS_INSTANCE(originalRequest);
+        return;
       }
 
-      return Promise.reject(error);
+      Promise.reject(error);
     },
   );
 
@@ -51,9 +43,30 @@ export const customInstance = <T>(
   return promise;
 };
 
-const refreshAccessToken = async () => {
-  await authControllerSignAccessToken({});
-};
+const API_SERVER_BASE_URL = process.env.REACT_APP_API_BASE_URL ?? '';
+const authStorePersisted = usePersistedAuthStore;
+
+export const AXIOS_INSTANCE = Axios.create({
+  baseURL: API_SERVER_BASE_URL,
+  withCredentials: true,
+}); // use your own URL here or environment variable
+
+export const AXIOS_INSTANCE_REFRESH_TOKEN = Axios.create({
+  baseURL: API_SERVER_BASE_URL,
+  withCredentials: true,
+}); // use your own URL here or environment variable
+
+AXIOS_INSTANCE_REFRESH_TOKEN.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: any) => {
+    if (error.response?.status === 401) {
+      authStorePersisted.persist.clearStorage();
+      window.location.href = '/signin';
+    }
+  },
+);
 
 // In some case with react-query and swr you want to be able to override the return error type so you can also do it here like this
 export type ErrorType<Error> = AxiosError<Error>;
