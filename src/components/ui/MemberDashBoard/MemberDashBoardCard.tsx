4df09ad7dc4e790b22d3a6ac8fa2809a65ba07b9
dev-captain <luckydeveloper1113@gmail.com>
2022-11-15 08:43:17 +0300

    member dashboard card shadow added

--- src/components/ui/MemberDashBoard/MemberDashBoardCard.tsx
+++ src/components/ui/MemberDashBoard/MemberDashBoardCard.tsx
@@ -6,6 +6,7 @@ const MemberDashboardCard = forwardRef<BoxProps, 'div'>((props, ref) => {
       id="member-dashboard-card"
       display="flex"
       bgColor="rgba(255, 255, 255, 1)"
+      boxShadow="0px 10px 10px -5px rgba(0, 6, 14, 0.08), 0px 20px 25px -5px rgba(0, 6, 14, 0.14);"
       p={{ base: '1.5em', lg: '2em' }}
       borderRadius="24px"
       alignItems="start"
