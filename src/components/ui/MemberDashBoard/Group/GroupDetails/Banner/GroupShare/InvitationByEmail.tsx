f4f6b5e6b1ff6ad394f26ed8dc72187f0b8007c0
dev-captain <luckydeveloper1113@gmail.com>
2022-11-16 19:42:29 +0800

    Add send email button.

--- src/components/ui/MemberDashBoard/Group/GroupDetails/Banner/GroupShare/InvitationByEmail.tsx
+++ src/components/ui/MemberDashBoard/Group/GroupDetails/Banner/GroupShare/InvitationByEmail.tsx
@@ -1,44 +1,44 @@
 import * as React from 'react';
-// import { useForm } from 'hooks';
-// import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'hooks';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { FormControl } from '@chakra-ui/react';
 
 import { Common } from 'components';
-// import { InputGroupPropsI } from '../../../../../../common/InputGroup';
-// import groupInviteYupScheme from '../../../../../../../lib/yup/schemas/groupInviteYupScheme';
+import { InputGroupPropsI } from '../../../../../../common/InputGroup';
+import groupInviteYupScheme from '../../../../../../../lib/yup/schemas/groupInviteYupScheme';
 // import { usePersistedGroupStore } from '../../../../../../../lib/zustand';
 
 const InvitationByEmail: React.FC = () => {
   // const group = usePersistedGroupStore().activeGroup;
-  // const { handleSubmit, errors, setValue } = useForm({
-  //   resolver: yupResolver(groupInviteYupScheme),
-  // });
-  // const onChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-  //   setValue(e.target.name as any, e.target.value as any, {
-  //     shouldValidate: true,
-  //     shouldDirty: true,
-  //   });
-  // };
+  const { handleSubmit, errors, setValue } = useForm({
+    resolver: yupResolver(groupInviteYupScheme),
+  });
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    setValue(e.target.name as any, e.target.value as any, {
+      shouldValidate: true,
+      shouldDirty: true,
+    });
+  };
 
   /* eslint no-underscore-dangle: 0 */
   // const groupInviteUrl = `${window.location.origin}/invite-link?group_id=${group?.id}&private=${group?.private}`;
-  // const inputItems: InputGroupPropsI[] = [
-  //   {
-  //     placeholder: 'Enter email here',
-  //     label: 'Invite by email',
-  //     onChange,
-  //     type: 'text',
-  //     name: 'email',
-  //     errorMsg: errors?.email?.message,
-  //     autoFocus: false,
-  //     whiteMode: true,
-  //   },
-  // ];
-  // const handleUpdateGroupNameChanges = async (data: any) => {
-  //   /**
-  //    * TODO: We will use our own api point to send email with template
-  //    */
-  // };
+  const inputItems: InputGroupPropsI[] = [
+    {
+      placeholder: 'hello@mail.com',
+      label: 'Invite by email',
+      onChange,
+      type: 'text',
+      name: 'email',
+      errorMsg: errors?.email?.message,
+      autoFocus: false,
+      whiteMode: true,
+    },
+  ];
+  const handleUpdateGroupNameChanges = async () => {
+    /**
+     * TODO: We will use our own api point to send email with template
+     */
+  };
 
   return (
     <FormControl
@@ -51,9 +51,9 @@ const InvitationByEmail: React.FC = () => {
       as="form"
       autoComplete="off"
       w="full"
-      // onSubmit={handleSubmit(handleUpdateGroupNameChanges)}
+      onSubmit={handleSubmit(handleUpdateGroupNameChanges)}
     >
-      {/* <Common.InputItems inputItems={inputItems} /> */}
+      <Common.InputItems inputItems={inputItems} />
       <Common.ImpaktButton
         mt={{ md: 0, base: '10px' }}
         variant="black"
