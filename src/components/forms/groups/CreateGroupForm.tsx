54c01571bd179dccb5475da64a685db9de077169
dev-captain <luckydeveloper1113@gmail.com>
2022-12-05 19:02:34 +0800

    update "group creating setting"

--- src/components/forms/groups/CreateGroupForm.tsx
+++ src/components/forms/groups/CreateGroupForm.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { useForm } from 'hooks';
-import { Flex, FormControl } from '@chakra-ui/react';
+import keys from 'i18n/types';
+import { useTranslation } from 'react-i18next';
+import { Flex, FormControl, Box, Button, Text, Tooltip } from '@chakra-ui/react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigate } from 'react-router-dom';
 import { Common, I } from 'components';
@@ -13,11 +16,14 @@ import { usePersistedAuthStore, usePersistedGroupStore } from '../../../lib/zust
 const CreateGroupForm: React.FC<{ onClose: (() => void) | undefined }> = ({ onClose }) => {
   const createGroup = useGroupsControllerV1Create();
   const { addToMyGroups } = usePersistedGroupStore();
-
+  const [value, settingValue] = useState('Public');
+  const [info, setInfo] = useState(false);
   const { handleSubmit, errors, setValue } = useForm({
     resolver: yupResolver(createGroupYupScheme),
     defaultValues: { groupName: '' },
   });
+  const { t } = useTranslation().i18n;
+  const helperText = t(keys.Message.PublicToolTip.description);
 
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     setValue(e.target.name as any, e.target.value as any, { shouldValidate: true });
@@ -65,6 +71,11 @@ const CreateGroupForm: React.FC<{ onClose: (() => void) | undefined }> = ({ onCl
       whiteMode: true,
     },
   ];
+  const toggleInfo = () => {
+    if (!info) {
+      setInfo(true);
+    } else setInfo(false);
+  };
 
   return (
     <FormControl
@@ -80,6 +91,88 @@ const CreateGroupForm: React.FC<{ onClose: (() => void) | undefined }> = ({ onCl
       w="full"
     >
       <Common.InputItems inputItems={inputItems} />
+      <Box
+        overflow="auto"
+        width="full"
+        paddingRight="8px"
+        css={{
+          '&::-webkit-scrollbar': {
+            width: '4px',
+          },
+          '&::-webkit-scrollbar-track': {
+            width: '6px',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            visibility: 'initial',
+            width: '10px',
+            background: '#D3E2F0',
+            borderRadius: '24px',
+          },
+        }}
+      >
+        <Box border="2px solid #EEF4F6" p="16px" borderRadius="16px" mb="16px">
+          <Box display="flex" justifyContent="space-between" alignItem="center">
+            <Text color="#29323B" fontSize={{ md: '18px', base: '12px' }} fontWeight="500">
+              Is your group public or private?
+            </Text>
+            <Text
+              color="#CC4C33"
+              display="block"
+              style={{ cursor: 'pointer' }}
+              onClick={toggleInfo}
+            >
+              learn more
+            </Text>
+          </Box>
+          <Box display="flex" width="100%" mt="12px">
+            <Button
+              color={value === 'Public' ? '#29323B' : '#728BA3'}
+              bg={value === 'Public' ? '#EEF4F6' : '#fff'}
+              _hover={{
+                backgroundColor: value === 'Private' ? 'transparent' : '#EEF4F6',
+                color: value === 'Private' ? '#728BA3' : '#29323B',
+              }}
+              _focus={{ boxShadow: 'none' }}
+              w="120px"
+              h="38px"
+              borderRadius="8px"
+              onClick={() => {
+                settingValue('Public');
+              }}
+            >
+              Public
+            </Button>
+            <Button
+              bg={value === 'Private' ? '#EEF4F6' : '#fff'}
+              color={value === 'Private' ? '#29323B' : '#728BA3'}
+              _hover={{
+                backgroundColor: value === 'Public' ? 'transparent' : '#EEF4F6',
+                color: value === 'Public' ? '#728BA3' : '#29323B',
+              }}
+              _focus={{ boxShadow: 'none' }}
+              w="120px"
+              h="38px"
+              borderRadius="8px"
+              onClick={() => {
+                settingValue('Private');
+              }}
+            >
+              Private
+            </Button>
+          </Box>
+          {info && (
+            <Text
+              color="#4E6070"
+              bg="#EEF4F6"
+              padding="15px"
+              style={{ display: 'block', marginTop: '20px', borderRadius: '10px' }}
+              dangerouslySetInnerHTML={{
+                __html: helperText,
+              }}
+            />
+          )}
+        </Box>
+      </Box>
       <Flex justifyContent="space-between" w="full">
         <Common.ImpaktButton
           variant="transparent"
