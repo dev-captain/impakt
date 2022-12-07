e39616c87cbcf3605f29ccd5bdeb6333c70cfa78
dev-captain <luckydeveloper1113@gmail.com>
2022-12-07 11:52:43 +0300

    temp nav fix

--- src/components/core/Navbar/NavBarLink.tsx
+++ src/components/core/Navbar/NavBarLink.tsx
@@ -17,9 +17,9 @@ const NavbarLink = ({ IsHeader }: Props) => {
   const path = parsePathname(location.pathname);
   const [isLessThan1040] = useMediaQuery('(max-width: 1040px)');
 
-  const textColor = location.pathname.includes('d') ? 'fitnessGray' : 'glass.100';
-  const activeColor = location.pathname.includes('d') ? 'fitnessGray' : 'glass.100';
-  const passiveColor = location.pathname.includes('d') ? 'fitnessGray' : 'rgba(255,255,255)';
+  const textColor = location.pathname === '/d' ? 'fitnessGray' : 'glass.100';
+  const activeColor = location.pathname === '/d' ? 'fitnessGray' : 'glass.100';
+  const passiveColor = location.pathname === '/d' ? 'fitnessGray' : 'rgba(255,255,255)';
 
   useEffect(() => {
     if (!isLessThan1040) {
