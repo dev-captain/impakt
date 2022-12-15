fa9cb5103151f9e8adf134c92d94cc003a23d30d
dev-captain <luckydeveloper1113@gmail.com>
2022-12-15 20:25:54 +0800

    welcome group modal

--- src/components/ui/MemberDashBoard/Group/GroupDetails/Content/Forums/Forums.tsx
+++ src/components/ui/MemberDashBoard/Group/GroupDetails/Content/Forums/Forums.tsx
@@ -30,11 +30,7 @@ const Forums: React.FC = () => {
 
   return (
     <>
-      <Box
-        marginStart="0 !important"
-        width={{ base: '100%', md: '40%', lgx: '50%' }}
-        display="flex"
-      >
+      <Box marginStart="0 !important" width={{ base: '100%' }} display="flex">
         <MemberDashboardCard p={{ base: '16px', md: '24px' }} marginLeft="auto">
           <Box w="full">
             <Box display="flex" justifyContent="space-between" alignItems="center" width="100%">
--- src/components/ui/MemberDashBoard/Group/GroupDetails/GroupDetails.tsx
+++ src/components/ui/MemberDashBoard/Group/GroupDetails/GroupDetails.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import { Box, Text, HStack, CircularProgress } from '@chakra-ui/react';
-import { usePersistedGroupStore } from 'lib/zustand';
 import Content from './Content/Content';
 import Banner from './Banner/Banner';
 import { useFetchGroupDetails } from '../../../../../hooks/useFetchGroupDetails';
-import GroupWelcome from '../../GroupWelcome';
 // import { getDefaultQueryOptions } from '../../../../../lib/impakt-dev-api-client/utils';
 
 const GroupDetails: React.FC = () => {
   const { group, isError, isGroupDetailsLoading } = useFetchGroupDetails();
-  const { role } = usePersistedGroupStore();
-  const [show, setShow] = React.useState<null | string>(null);
-  React.useEffect(() => {
-    const showTip = localStorage.getItem('showTip');
-    if (showTip) {
-      setShow(showTip);
-    }
-  }, []);
-
-  const hide = () => {
-    localStorage.setItem('showTip', 'false');
-    setShow('false');
-  };
-
   if (isError.length > 0)
     return (
       <Text fontWeight="hairline" fontSize="2xl">
@@ -34,14 +18,10 @@ const GroupDetails: React.FC = () => {
 
   return (
     <HStack w="full" as="section" id="group-detail-section">
-      {(!localStorage.getItem('showTip') || !show) && role === 'Creator' ? (
-        <GroupWelcome hideGroupWelcome={hide} />
-      ) : (
-        <Box maxW="1200px" w="100%">
-          <Banner />
-          <Content />
-        </Box>
-      )}
+      <Box maxW="1200px" w="100%">
+        <Banner />
+        <Content />
+      </Box>
     </HStack>
   );
 };
