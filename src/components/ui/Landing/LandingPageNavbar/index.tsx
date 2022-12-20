3b2fae8190165ddd8f54aadaf7744d8f4a9d6e58
dev-captain <luckydeveloper1113@gmail.com>
2022-12-20 13:37:21 +0300

    display flex without hook

--- src/components/ui/Landing/LandingPageNavbar/index.tsx
+++ src/components/ui/Landing/LandingPageNavbar/index.tsx
@@ -126,7 +126,7 @@ const LandingPageNavbar: FC = () => {
           </HStack>
 
           <Common.CollapseMenuIconButton
-            display={[isLessThan1280 ? 'flex' : 'none']}
+            display={{ base: 'flex', lg: 'none' }}
             aria-label="Toggle Navigation"
             isOpen={isOpen}
             onClick={onToggle}
