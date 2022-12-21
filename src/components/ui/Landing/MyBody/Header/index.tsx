1d65135a4f9f48eb7081e126e55ba7856169c192
dev-captain <luckydeveloper1113@gmail.com>
2022-12-21 13:44:02 +0300

    my body section text updated

--- src/components/ui/Landing/MyBody/Header/index.tsx
+++ src/components/ui/Landing/MyBody/Header/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Box } from '@chakra-ui/react';
 import MyBodyDescriptionText from './MyBodyDescriptionText';
 import MyBodyHeadlineText from './MyBodyHeadlineText';
+import { Common } from '../../../..';
 
 const MyBodyHeader: React.FC = () => {
   return (
@@ -16,6 +17,17 @@ const MyBodyHeader: React.FC = () => {
         <MyBodyHeadlineText />
         <Box height="1px" background="#000" width="152px" margin="24px auto" />
         <MyBodyDescriptionText />
+        <Box mt="1em">
+          <Common.ImpaktButton
+            variant="black"
+            height="50px"
+            w="fit-content"
+            gap="8px"
+            padding="10px 14px"
+          >
+            Experience vSports now in your browser
+          </Common.ImpaktButton>
+        </Box>
       </Box>
     </Box>
   );
