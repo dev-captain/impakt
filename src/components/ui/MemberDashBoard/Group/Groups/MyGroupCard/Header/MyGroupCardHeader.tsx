e354d627de39224d05298978658b3dba5c4ec443
dev-captain <luckydeveloper1113@gmail.com>
2022-12-28 07:41:39 +0300

    feat: explore button added on my groups header

--- src/components/ui/MemberDashBoard/Group/Groups/MyGroupCard/Header/MyGroupCardHeader.tsx
+++ src/components/ui/MemberDashBoard/Group/Groups/MyGroupCard/Header/MyGroupCardHeader.tsx
@@ -1,15 +1,17 @@
-import { Box } from '@chakra-ui/react';
+import { HStack } from '@chakra-ui/react';
 import React from 'react';
 
 import GroupCardWrapperHeader from '../../GroupCardHeader';
 import CreateGroupButton from '../CreateGroupButton';
+import ExploreGroupButton from '../ExploreGroupButton';
 
 const MyGroupCardHeader: React.FC = () => {
   return (
     <GroupCardWrapperHeader justify="space-between" title="My Groups">
-      <Box mb="20px">
+      <HStack mb="20px">
+        <ExploreGroupButton />
         <CreateGroupButton />
-      </Box>
+      </HStack>
     </GroupCardWrapperHeader>
   );
 };
